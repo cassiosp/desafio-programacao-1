4df803fa3725f78f1054fb264766911dc2501f95
Cassio Pereira <cassio@shockmonkeystudios.com>
2014-10-01 17:50:38 -0300

    Formulário básico de upload e remoção de rotas inoperantes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   get 'vendas_importacao/index'
 
-  get 'sales_import/import'
-
-  get 'sales_import/' => 'sales_import#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
