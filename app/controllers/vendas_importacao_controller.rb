fe0bd64de033501a75bab2cbc026c09cd518994f
Cassio Pereira <cassio@shockmonkeystudios.com>
2014-10-03 12:09:52 -0300

    Gravação dos registros em banco de dados e listagem de registros gravados.

--- app/controllers/vendas_importacao_controller.rb
+++ app/controllers/vendas_importacao_controller.rb
@@ -1,62 +1,70 @@
 require 'csv'
 
 class VendasImportacaoController < ApplicationController
+  def index
+    @vendas = Venda.all
+  end
+
   def importacao
   end
 
   def upload
-    
+
+    @vendas = []
+
     salvar_arquivo_enviado
-    
+
     processar_arquivo
 
-    guardar_linhas_processadas
+    mostrar_linhas_processadas
 
   end
-  
+
   private
-  
+
   def processar_arquivo
-    
+
     caminho = Rails.root.join('public', 'uploads', @arquivo_enviado.original_filename)
     coluna = nil
     CSV.foreach caminho, {:col_sep => "\t"} do |linha|
       puts "Colunas => #{linha}" if linha.include? "purchaser name"
       puts "Linha => #{linha}" unless linha.include? "purchaser name"
-      
+
       if coluna
         venda = Venda.new
         coluna.each_index do |index|
-          puts coluna[index].class
-          venda.send("#{coluna[index].gsub(" ", "_")}=".to_sym, venda[index])  
+          metodo = "#{coluna[index].gsub(" ", "_")}=".to_sym
+          puts "#{metodo} => #{linha[index]} => #{index}"
+          venda.send(metodo, linha[index])
         end
-        
+        @vendas << venda if venda.save
       end
 
       coluna ||= linha
 
     end
-    
+
   end
-  
-  def salvar_arquivo_enviado 
+
+  def salvar_arquivo_enviado
     @arquivo_enviado = params[:arquivo]
     File.open(Rails.root.join('public', 'uploads', Time.now.to_i.to_s.concat('_').concat(@arquivo_enviado.original_filename)), 'wb') do |arquivo|
       arquivo.write(@arquivo_enviado.read)
     end
   end
-  
-  def guardar_linhas_processadas
+
+  def mostrar_linhas_processadas
+
+    puts @vendas
+
     @linhas_lidas = []
     File.open(Rails.root.join('public', 'uploads', @arquivo_enviado.original_filename), 'rb') do |arquivo|
 
       arquivo.each do |linha|
-      # puts "linha.ascii_only? = #{linha.ascii_only?}"
         @linhas_lidas << linha.force_encoding('utf-8')
-      # @linhas_lidas << linha.encode('utf-8', 'ASCII-8BIT', :undef => :replace)
       end
 
     end
   end
-    
+
 end
--- app/helpers/vendas_importacao_helper.rb
+++ app/helpers/vendas_importacao_helper.rb
@@ -1,11 +1,2 @@
 module VendasImportacaoHelper
-  
-  class LeitorArquivoVendas
-    
-    def linha_para_venda(linha)
-      linha =~ /./
-    end
-    
-  end
-  
 end
